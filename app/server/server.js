253db17d839439c77736017d7f5910736a72d1ae
jonnyMitts <jmittelbronn@gmail.com>
2013-07-02 01:05:35 -0400

    got the mongo working

--- app/server/server.js
+++ app/server/server.js
@@ -1,43 +1,24 @@
-var app = require('http').createServer(handler)
-  , io = require('socket.io').listen(app)
-  , fs = require('fs');
+var app = require('http').createServer(handler),
+  io = require('socket.io').listen(app),
+  fs = require('fs'),
+  mainSocket;
 
-var MongoClient = require('mongodb').MongoClient
-    , format = require('util').format;    
-
+var databaseUrl = "mongodb://127.0.0.1:27017/test"; // "username:password@example.com/mydb"
+var collections = ["contest"]
+var db = require("mongojs").connect(databaseUrl, collections);
 
 app.listen(3000);
+io.sockets.on('connection', function(socket) {
+  db.contest.find({
+    "practice": "Atlanta ENT"
+  }, function(err, practice) {
+    socket.on("getOffices", function(data) {
+      socket.emit('offices', JSON.stringify(practice));
+    })
 
-var request;
-function handler (req, res) {
-	
-  fs.readFile(__dirname + req.url,
-  function (err, data) {
-    console.log("Helloooooooo")
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading ' + req.url);
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
-
-io.sockets.on('connection', function (socket) {
-  socket.on('getOffices', function (data) {
-        MongoClient.connect('mongodb://127.0.0.1:27017/contest', function(err, db) {
-          console.log("err: " + err);
-          if(err) throw err;
-          
-          //var collection = db.use('contest');
-          console.log(db);
-          socket.emit('offices',db)
-  //         socket.broadcast.emit('offices',JSON.stringify(collection));
-      });
   });
 });
 
+function handler(req, res) {
 
-
-  
+}
